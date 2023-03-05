3df3eef027d99923c7db431ef82cd0b33d2e1534
Dark Screen Shield <1142561578@qq.com>
2023-03-05 10:45:43 +0800

    optimize the focal loss

--- WTV/model/loss.py
+++ WTV/model/loss.py
@@ -12,12 +12,12 @@ class FocalLoss(nn.Module):
         self.gamma=gamma
         self.size_average=size_average
         if alpha is None:
-            self.alpha=Variable(torch.tensor([0.00,50,1,1,20]))
+            self.alpha=Variable(torch.tensor([0,1,2,3,4])).float()
         else:
             if isinstance(alpha,Variable):
-                self.alpha=alpha
+                self.alpha=alpha.float()
             else:
-                self.alpha=Variable(alpha)
+                self.alpha=Variable(alpha).float()
         # self.alpha=self.alpha.cuda()
 
     def forward(self,outputs,targets):
@@ -25,7 +25,7 @@ class FocalLoss(nn.Module):
         # C for class size
         N = outputs.size(0)
         C = outputs.size(1)
-        P = outputs.softmax(dim=1)
+        P = outputs.softmax(dim=1).float()
 
         # 将target转成one-hot编码
         class_mask = outputs.data.new(N, C).fill_(0)
@@ -37,15 +37,22 @@ class FocalLoss(nn.Module):
 
         if outputs.is_cuda and not self.alpha.is_cuda:
             self.alpha=self.alpha.cuda()
+        # alpha_size (batchsize,) 1d
         alpha = self.alpha[ids.data.view(-1)]
+        # probs size (batchsize,1) 2d
         probs = (P * class_mask).sum(1).view(-1, 1)
         log_p=probs.log()
-        batch_loss=-alpha*torch.pow((1-probs),self.gamma)*log_p
-
+        # nothing=alpha*log_p
+        # something=torch.pow((1 - probs), self.gamma) * log_p
+        # batch_loss=-alpha*torch.pow((1-probs),self.gamma)*log_p
+        batch_loss=torch.dot(-alpha,(torch.pow((1 - probs), self.gamma) * log_p).squeeze())
+        # dot_product = torch.dot(-alpha,something.squeeze())
         if self.size_average:
-            loss=batch_loss.mean()
+            # loss=batch_loss.mean()
+            loss=batch_loss/N
         else:
-            loss=batch_loss.sum()
+            # loss=batch_loss.sum()
+            loss=batch_loss
         return loss
 
 
@@ -53,8 +60,8 @@ class FocalLoss(nn.Module):
 
 
 if __name__ == "__main__":
-    output = torch.tensor([[0.9, 0, 0, 0, 0], [0, 0.89, 0, 0, 0]])
-    target = torch.tensor([0, 2])
+    output = torch.tensor([[0.9, 0, 0, 0, 0], [0, 0.89, 0, 0, 0],[0,0,3,1,1]])
+    target = torch.tensor([0, 1,2])
     lossfun=FocalLoss()
     loss=lossfun(output,target)
     print("focal loss:",loss)
--- test/test_tensorboard.py
+++ test/test_tensorboard.py
@@ -2,22 +2,22 @@ import torch
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 
-# 创建一个SummaryWriter对象，用于记录日志
-
-writer = SummaryWriter("./log")
-
-# 创建一些虚构数据并进行训练
-x = torch.linspace(-5, 5, 100)
-y1 = F.relu(x)
-y2 = F.relu(-x)
-y=torch.stack([y1,y2],dim=1)
-a=y[1]
-# print(y[:][1])
-# print(y.shape)
-for i in range(100):
-    # writer.add_scalar('relu', y1[i], i)
-    # writer.add_scalar('relu', y2[i], i)
-    writer.add_scalar('relu', y[i], i)
-
-# 关闭SummaryWriter对象
-writer.close()
+# # 创建一个SummaryWriter对象，用于记录日志
+#
+# writer = SummaryWriter("./log")
+#
+# # 创建一些虚构数据并进行训练
+# x = torch.linspace(-5, 5, 100)
+# y1 = F.relu(x)
+# y2 = F.relu(-x)
+# y=torch.stack([y1,y2],dim=1)
+# a=y[1]
+# # print(y[:][1])
+# # print(y.shape)
+# for i in range(100):
+#     # writer.add_scalar('relu', y1[i], i)
+#     # writer.add_scalar('relu', y2[i], i)
+#     writer.add_scalar('relu', y[i], i)
+#
+# # 关闭SummaryWriter对象
+# writer.close()
